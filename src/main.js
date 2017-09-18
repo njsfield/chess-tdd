37b762d012f1c4a287cd9ccf4a56802269ee8ea7
Nick Field <heyimnick@live.com>
2017-09-18 12:32:16 +0100

    add react

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter';
+
+// Main render
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(Counter),
+    document.getElementById('main')
+  );
+});
