2c06ce9b16efddeb34ea8ca0af6e957faf34d0e5
Nick Field <heyimnick@live.com>
2017-09-18 14:52:39 +0100

    add css config

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './Counter';
 
+// import main styles
+import './styles/index.css';
+
 // Main render
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   context: path.join(__dirname, 'src'), // Path of client-side code directory
@@ -14,9 +15,19 @@ module.exports = {
         test: /\.js$/, // If .js extension then
         exclude: /node_modules/, // (but not node_modules)
         use: ['babel-loader'] // Run through babel
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader']
+        })
       }
     ]
   },
+  plugins: [
+    new ExtractTextPlugin('../css/style.css') // outputs css to public directory (relative to output path)
+  ],
   resolve: {
     modules: [path.join(__dirname, 'node_modules')] // Where should webpack look for files referenced by import/require?
   }
